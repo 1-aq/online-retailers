d9a5587e090eec56479170c202f6d160b29d6776
lianqi <3546065570@qq.com>
2022-06-25 21:04:26 +0800

    docs(ad): addd

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>dasdasdasd</h1>
   </div>
 </template>
 
@@ -12,7 +13,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
  *
  */
 export default {
-  name: "HomeView",
+  name: 'HomeView',
   components: {
     HelloWorld
   }
